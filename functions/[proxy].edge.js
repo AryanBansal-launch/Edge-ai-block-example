5c6ce9070ef4dde8c8a36d6cf2e4d9a24765ea5c
Aryan Bansal <aryan.bansal@contentstack.com>
2025-07-18 18:18:31 +0530

    Working edge AI blocker

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,44 +1,25 @@
-// const KNOWN_BOTS = [
-//     'claudebot',
-//     'gptbot',
-//     'googlebot',
-//     'bingbot',
-//     'ahrefsbot',
-//     'yandexbot',
-//     'semrushbot',
-//     'mj12bot',
-//     'facebookexternalhit',
-//     'twitterbot',
-//   ];
+const KNOWN_BOTS = [
+    'claudebot',
+    'gptbot',
+    'googlebot',
+    'bingbot',
+    'ahrefsbot',
+    'yandexbot',
+    'semrushbot',
+    'mj12bot',
+    'facebookexternalhit',
+    'twitterbot',
+  ];
   
-//   export default function handler(request) {
-//     const userAgent = (request.headers.get('user-agent') || '').toLowerCase();
+  export default function handler(request) {
+    const userAgent = (request.headers.get('user-agent') || '').toLowerCase();
   
-//     const isBot = KNOWN_BOTS.some(bot => userAgent.includes(bot));
+    const isBot = KNOWN_BOTS.some(bot => userAgent.includes(bot));
   
-//     if (isBot) {
-//       console.warn(`:no_entry: Blocked bot: UA="${userAgent}"`);
-//       return new Response('Forbidden: AI crawlers are not allowed.', { status: 403 });
-//     }
-  
-//     return fetch(request);
-//   }
-
-export default async function handler(request) {
-    const redirectHosts = [
-      'edge-ai-block-example.devcontentstackapps.com/',
-      'edge-ai-block-example.devcontentstackapps.com/123'
-    ];
-  
-    const url = new URL(request.url);
-    console.log('Request URL:', url.toString());
-    console.log('hostname:', url.hostname);
-    if (redirectHosts.includes(url.hostname)) {
-      url.hostname = 'pranav-joshi.csnonprod.com';
-      console.log('Redirecting to:', url.toString());
-      return Response.redirect(url.toString(), 308); // permanent redirect
+    if (isBot) {
+      console.warn(`:no_entry: Blocked bot: UA="${userAgent}"`);
+      return new Response('Forbidden: AI crawlers are not allowed.', { status: 403 });
     }
   
-    // Continue with normal request
     return fetch(request);
   }
